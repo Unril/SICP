1bf1fdb78db31cc8349a2888cb0079b6dcb4c6b0
unril <xunrilx@gmail.com>
2014-04-24 14:42:44 +0400

    no message

--- Exercises/Ch2/Ex2.37.rkt
+++ Exercises/Ch2/Ex2.37.rkt
@@ -1,2 +1,39 @@
 #lang racket
 
+(define test-mat (list (list 1 2 3)
+                       (list 4 5 6)
+                       (list 7 8 9)))
+
+(define test-vec-1 (list 1 2 3))
+(define test-vec-2 (list 1 10 100))
+
+{define (dot-product v1 v2)
+  (apply + (map * v1 v2))}
+
+; 321
+(dot-product test-vec-1 test-vec-2)
+
+{define (mat-*-vec mat vec)
+  (map {λ (row)
+         (apply + 
+                (map * 
+                     row
+                     vec))}
+       mat)}
+
+; (321, 654, 987)
+(mat-*-vec test-mat test-vec-2) 
+
+{define (transpose mat)
+  (apply map list mat)}
+
+; {{1, 4, 7}, {2, 5, 8}, {3, 6, 9}}
+(transpose test-mat) 
+
+{define (mat-*-mat mat-1 mat-2)
+  (map (curry mat-*-vec
+              (transpose mat-2))
+       mat-1)}
+
+; {{30, 36, 42}, {66, 81, 96}, {102, 126, 150}}
+(mat-*-mat test-mat test-mat)
